bcad070d1bcac074213c05697d59e98c21a29bbd
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2022-04-07 00:29:39 +0200

    rename variables

--- x10.py
+++ x10.py
@@ -1,24 +1,24 @@
 import PyPDF2
 import sys
 
-filename_in = sys.argv[1]
-filename_out = filename_in
-pages = int(sys.argv[2])
-template = sys.argv[3] if len(sys.argv) >= 4 else None
+FILENAME_IN = sys.argv[1]
+FILENAME_OUT = FILENAME_IN
+PAGES = int(sys.argv[2])
+TEMPLATE = sys.argv[3] if len(sys.argv) >= 4 else None
 
-pdf_reader = PyPDF2.PdfFileReader(filename_in)
+pdf_reader = PyPDF2.PdfFileReader(FILENAME_IN)
 pdf_writer = PyPDF2.PdfFileWriter()
 
-if template is not None:
-    template_reader = PyPDF2.PdfFileReader(template)
+if TEMPLATE is not None:
+    template_reader = PyPDF2.PdfFileReader(TEMPLATE)
     page = template_reader.getPage(0)
 else:
     page = pdf_reader.getPage(-1)
 
 for n in range(0, pdf_reader.getNumPages()):
     pdf_writer.addPage(pdf_reader.getPage(n))
-for n in range(1, pages+1):
+for n in range(1, PAGES+1):
     pdf_writer.addPage(page)
 
-with open(filename_out, 'wb') as output_file:
+with open(FILENAME_OUT, 'wb') as output_file:
     pdf_writer.write(output_file)
